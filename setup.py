00fe2b01fa610582fe576c56e5be4a54c0f71155
Ciro Gallo <58234348+Ciro-Gallo@users.noreply.github.com>
2020-11-08 11:46:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'it_text_cleaner',         # How you named your package folder (MyLib)
   packages = ['it_text_cleaner'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Cleans text in italian language. Designed for machine learning.',   # Give a short description about your library
   author = 'Ciro Gallo',                   # Type in your name
